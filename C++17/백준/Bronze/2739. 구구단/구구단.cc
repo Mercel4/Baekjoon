f8d8d78c2d97cf24c67ee60abe79affcf858abda
AN JAE MIN <jaeminan944@icloud.com>
2026-03-05 20:09:59 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int dan;
+    std::cin >> dan;
+
+    for (int i = 1; i <= 9; ++i)
+    {
+        std::cout << dan << " * " << i << " = " << dan * i << std::endl;
+    }
+
+    return 0;
+}
