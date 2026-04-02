176e1753a7524f3db147992dd01c5c4a3241be66
AN JAE MIN <jaeminan944@icloud.com>
2026-04-02 13:18:36 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "         ,r'\"7" << std::endl; // 큰따옴표 앞에 \를 붙여서 출력
+    std::cout << "r`-_   ,'  ,/" << std::endl;
+    std::cout << " \\. \". L_r'" << std::endl;      // \ 하나를 위해 \\ 사용
+    std::cout << "   `~\\/" << std::endl;        // \ 하나를 위해 \\ 사용
+    std::cout << "      |" << std::endl;
+    std::cout << "      |" << std::endl;
+
+    return 0;
+}
